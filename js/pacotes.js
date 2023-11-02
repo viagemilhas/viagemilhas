af0ea128518ce6ed4d6a5dbd217c96f91cc6fc8e
luis henrique <duimbispo@gmail.com>
2023-11-02 19:50:38 -0300

    Criação da página pacotes e adicionamento do hover nos cards

---
+++ js/pacotes.js
@@ -0,0 +1,6 @@
+// Esta funcão redireciona todos os cards da página pacotes para a página de confirmação, quando
+// o usuario clica em qualquer card
+
+function redirecionamento(){
+    window.location.href = "index.html";
+}
