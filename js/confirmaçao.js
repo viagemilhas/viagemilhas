1d5d15cb35f7113d83a78edffa48335387d03d10
luis henrique <duimbispo@gmail.com>
2023-11-02 21:14:55 -0300

    Criação e finalização da tela de confirmação

--- js/confirmaçao.js
+++ js/confirmaçao.js
@@ -1,6 +1,6 @@
 // Esta funcão redireciona todos os cards da página pacotes para a página de confirmação, quando
 // o usuario clica em qualquer card
 
-function redirecionamento(){
-    window.location.href = "confirmaçao.html";
+function voltarhome(){
+    window.location.href = "index.html";
 }
