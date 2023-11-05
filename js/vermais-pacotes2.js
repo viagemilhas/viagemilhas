1d62f770e2313edac1b4262a9a6c1377ad62f895
luis henrique <duimbispo@gmail.com>
2023-11-05 00:43:37 -0300

    Ajustes nos cards ver mais da página pacotes

---
+++ js/vermais-pacotes2.js
@@ -0,0 +1,7 @@
+function verMaispacotes2(){
+    let verCards2 = document.getElementsByClassName("card-pacote-vermais2");
+    console.log(verCards2)
+    for (i = 0; i < verCards2.length; i++) {
+        verCards2[i].style.display = "block";
+    }
+}
